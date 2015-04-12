44b9802d2fd58a5a227b1ddb0a4e1c7f1dc8733b
Bartek <bartekskorulski@gmail.com>
2015-04-12 15:12:59 +0200

    adds web-mode for erb

--- html-conf.el
+++ html-conf.el
@@ -1,3 +1,17 @@
 (add-to-list 'auto-mode-alist '("\\.css.scss\\'" . css-mode))
 
 (add-hook 'css-mode-hook 'rainbow-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'ac-modes 'web-mode)
+(add-hook 'web-mode-hook
+          (lambda () (flyspell-prog-mode)))
+
+(add-hook 'web-mode-hook #'(lambda () (smartparens-mode -1)))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
